73ce5c4b16c00b5894c35b0059fdec61b0cb1329
Denis Burdin <dionisiy.god.of.wine@gmail.com>
2024-10-10 14:43:48 +0300

    task1 fix

--- js/task_1.js
+++ js/task_1.js
@@ -1,9 +1,6 @@
 const widthInput = document.querySelector('.width')
 const heightInput = document.querySelector('.height')
 
-console.log(document.querySelector('.area'));
-
-
 const calculate = ()=> {
     validation(widthInput.value, heightInput.value)
     const height = heightInput.value
@@ -14,26 +11,21 @@ const calculate = ()=> {
         perimetr: height*2+width*2,
         diagonale: (height**2+width**2)**0.5
     }
-    
     return result
 }
 
 const validation = (width, height) =>{
    
 if(width < 0 || height < 0 ){
-    window.alert('Хаха, це так смішно))))')
     heightInput.value = 0
     widthInput.value = 0
-
-    return false
+    window.open("https://letmegooglethat.com/?q=%D0%A7%D0%B8%D0%BC+%D0%B2%D1%96%D0%B4%D1%80%D1%96%D0%B7%D0%BD%D1%8F%D1%8E%D1%82%D1%8C%D1%81%D1%8F+%D0%B4%D0%BE%D0%B4%D0%B0%D1%82%D0%BD%D1%96+%D1%96+%D0%B2%D1%96%D0%B4%D1%94%D0%BC%D1%96+%D1%87%D0%B8%D1%81%D0%BB%D0%B0%3F",  "_blank")
+    window.alert('Хаха))))')
 }
-return true
 }
 
 const setResult = ()=> {
     const result = calculate()
-    console.log(result)
-    
     document.querySelector('.perimetr').textContent = result.perimetr
     document.querySelector('.area').textContent = result.area
     document.querySelector('.diagonale').textContent = result.diagonale
